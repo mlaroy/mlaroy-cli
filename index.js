8eddffb91cf23a6bbb2bc08348d6d9edd2b68750
Michael LaRoy <mike.laroy@gmail.com>
2022-01-17 09:53:01 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+console.log(`
+Michael LaRoy
+CEO Cascadia Digital
+
+🐦  Twitter: https://twitter.com/laroymike
+📖  Github: https://github.com/mlaroy
+🖥️   Blog: https://mikelaroy.ca
+
+`);
