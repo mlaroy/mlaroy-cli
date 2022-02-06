8c09d1308b01b46e45d271c223fefba92b3257b1
Michael LaRoy <mike.laroy@gmail.com>
2022-02-05 18:25:54 -0800

    add welcome package

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
-
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome')
 
 // clears the console
-clearConsole();
+welcome({
+    title: pkgJSON.name,
+    tagLine: 'Get to know Michael',
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: '#78A9C9',
+    // color: '#78A9C9',
+    bold: true,
+    clear: true
+})
 
 console.log(`
 Michael LaRoy
